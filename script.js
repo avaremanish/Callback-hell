87cdcac229d52bb18ed3987955b80d05fa125797
avaremanish <95077592+avaremanish@users.noreply.github.com>
2022-03-21 15:52:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const countdown= document.querySelector(".countdown");
+let time=10;
+
+
+countdown.innerText = time--;
+setTimeout( () =>{
+    countdown.innerText = time--;
+    setTimeout( () =>{
+        countdown.innerText = time--;
+setTimeout( () =>{
+    countdown.innerText = time--;
+setTimeout( () =>{
+
+    countdown.innerText = time--;
+setTimeout( () =>{
+    countdown.innerText = time--;
+setTimeout( () =>{
+    countdown.innerText = time--;
+setTimeout( () =>{
+    countdown.innerText = time--;
+    setTimeout( () =>{
+        countdown.innerText = time--;
+setTimeout( () =>{
+    countdown.innerText = time--;
+    setTimeout( () =>{
+        countdown.innerText = time--;
+setTimeout( () =>{
+             
+    countdown.innerText = "Happy Independence Day!";
+   
+                                            },1000);
+                                        },1000);
+                                      },1000);
+                                   },1000);
+                               }, 1000);  
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+             }, 1000);
+         }, 1000);
+     }, 2000);
+
+    
